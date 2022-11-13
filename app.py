6ec2249ac2f607d0f480b0e54ade4e59936f4ecc
tumainilyimo <lyimotumaini@gmail.com>
2022-11-13 11:13:09 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl")
+
+input_text = st.text_input("ask a question ")
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+outputs = model.generate(input_ids)
+st.write(tokenizer.decode(outputs[0]))
