6d778a906943e2d8bc3ce2c127c53b39381891ff
tumainilyimo <lyimotumaini@gmail.com>
2022-11-13 12:08:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large")
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
 
 input_text = st.text_input("ask a question ")
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids
